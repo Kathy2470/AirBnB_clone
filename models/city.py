adfb4d58c0c953a3f2c02154a8daf3973cb64b64
kathy2470 <mukisakathy@gmail.com>
2024-02-10 23:29:49 +0300

    update

--- models/city.py
+++ models/city.py
@@ -1,6 +1,7 @@
 from models.base_model import BaseModel
 
+
 class City(BaseModel):
-        """City class that inherits from BaseModel."""
-            state_id = ""
-                name = ""
+    """City class that inherits from Basemodel."""
+    state_id = ""
+    name = ""
