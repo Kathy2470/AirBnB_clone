f9023dd1671d40655d8b43ca03f4a72b435ee6ae
kathy2470 <mukisakathy@gmail.com>
2024-02-13 22:28:50 +0300

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 """Defines the FileStorage class."""
 import json
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
-
 
 class FileStorage:
     """Represent an abstracted storage engine.
@@ -39,11 +31,12 @@ class FileStorage:
     def reload(self):
         """Deserialize the JSON file __file_path to __objects, if it exists."""
         try:
-            with open(FileStorage.__file_path) as f:
-                objdict = json.load(f)
-                for o in objdict.values():
-                    cls_name = o["__class__"]
-                    del o["__class__"]
-                    self.new(eval(cls_name)(**o))
+            with open(FileStorage.__file_path, "r") as file:
+                obj_dict = json.load(file)
+                for key, val in obj_dict.items():
+                    _class = globals()[val['__class__']]
+                    obj_instance = _class(**val)
+                    FileStorage.__objects[key] = obj_instance
+                    print(f"Reload object: {obj_instance}")
         except FileNotFoundError:
-            return
+            pass
